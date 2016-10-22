47a8fbeb573276df19ec81cf04a4e6ea4881318e
hiro mbp <guest@guest.com>
2016-10-22 20:28:14 +0900

    [add] 初期プロジェクトファイルを追加

---
+++ src/HiddenPokemonSearcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiddenPokemonSearcher
+//
+//  Created by Hiroaki Fujisawa on 2016/10/22.
+//  Copyright © 2016年 xinext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
